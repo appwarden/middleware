909102597197eab2e30b8a889bb840a184ce483a
stevefrench39 <stevefrench39@protonmail.com>
2026-02-06 14:12:03 -0700

    feat!: remove Next.js Pages Router bundle
    BREAKING CHANGE: Remove the deprecated Next.js Pages Router bundle that was
    designed for @cloudflare/next-on-pages. This removes:
    
    - The withAppwardenOnNextJs export
    - The appwardenOnPagesNextJs runner function
    - The NextJsConfigFnOutputSchema and NextJsConfigFnType schemas
    
    Users should migrate to the modern Next.js Cloudflare bundle:
    import { createAppwardenMiddleware } from '@appwarden/middleware/cloudflare/nextjs'

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,6 +1,5 @@
 export * from "./cloudflare"
 export * from "./helpers"
-export * from "./nextjs"
 export * from "./use-appwarden"
 export * from "./use-content-security-policy"
 export * from "./vercel"
